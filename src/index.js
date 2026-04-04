42c6d99bb5c02d316cfa41573f68a824aa912063
Megha Patel <htmldesigner@penguinpeak.com>
2026-04-04 10:07:37 +0530

    Deployed nodejs application on aws eks

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("Hello this is Kubernets")
 })
+
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
 const PORT = process.env.PORT || 5000
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server started on port:${PORT}`);

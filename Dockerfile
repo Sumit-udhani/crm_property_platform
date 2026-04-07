43546a2d419159bef883461be10d482c7a93af1f
Megha Patel <htmldesigner@penguinpeak.com>
2026-04-07 16:18:02 +0530

    prisma issue

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,21 @@
 FROM node:22-alpine
 
 WORKDIR /app
+
 ARG DATABASE_URL
 ENV DATABASE_URL=$DATABASE_URL
-COPY package*.json .
+
+COPY package*.json ./
 RUN npm ci && npm cache clean --force
+
+COPY prisma ./prisma/
+
+
+RUN npx prisma generate
+
 COPY . .
 
-
 EXPOSE 5000
-CMD ["sh", "-c", "npx prisma generate && npx prisma db seed && node src/index.js"]
+
+
+CMD ["node", "src/index.js"]
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,11 @@
-import pkg from '@prisma/client';
+const { PrismaPg } = require('@prisma/adapter-pg')
+const { PrismaClient } = require('@prisma/client')
 
-const { PrismaClient } = pkg;
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+})
+
+const prisma = new PrismaClient({ adapter })
 
 async function main() {
   await prisma.roles.upsert({
@@ -12,7 +16,6 @@ async function main() {
       description: 'Super Admin with full access'
     }
   });
-
   console.log('✅ SUPER ADMIN role ensured');
 }
 

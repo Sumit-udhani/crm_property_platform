e82434936ff83f71ff9bd6cc78de65a5c8d4b01a
Megha Patel <htmldesigner@penguinpeak.com>
2026-04-08 18:29:13 +0530

    Called logout api

--- frontend/saasable-ui/admin/nextjs/src/layouts/AdminLayout/Header/HeaderContent/Profile.jsx
+++ frontend/saasable-ui/admin/nextjs/src/layouts/AdminLayout/Header/HeaderContent/Profile.jsx
@@ -77,6 +77,7 @@ export default function ProfileSection() {
     try {
       setAnchorEl(null);
        await authService.logout();
+       router.push('/login')
       
     } catch (error) {
       
